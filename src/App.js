bdf81573bac2829f4773edff0a47cec472c01a26
Mirza Bakti <mirza.bakti@gmail.com>
2022-07-14 17:21:36 +0700

    search and dropdown

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import Select from "react-select";
 import "./App.css";
-import { useEffect } from "react";
+import Select from "react-select";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+
   const getBerries = async () => {
     const berries = await fetch("https://pokeapi.co/api/v2/berry/");
     const value = await berries.json();
+    const result = value.results.map(data => {
+      return {
+        label: data.name,
+        value: data.name,
+      };
+    });
 
-    console.log(value);
+    setDatas(result);
   };
 
   useEffect(() => {
     getBerries();
   }, []);
 
-  return <div className="App">{/* <Select options={options}></Select> */}</div>;
+  return (
+    <div className="App">
+      <Select options={datas}></Select>
+    </div>
+  );
 }
 
 export default App;
