dc4cbb97eacc131091bac8c8ab5e98874561237b
Mirza Bakti <mirza.bakti@gmail.com>
2022-07-14 18:26:36 +0700

    showing output

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [datas, setDatas] = useState([]);
+  const [userSelect, setUserSelect] = useState("");
+  const [isShow, setIsShow] = useState(false);
 
   const getBerries = async () => {
     const berries = await fetch("https://pokeapi.co/api/v2/berry/");
@@ -22,9 +24,21 @@ function App() {
     getBerries();
   }, []);
 
+  const handleSubmit = () => {
+    setIsShow(state => !state);
+  };
+
+  const handleChange = (value) => {
+    setUserSelect(value);
+  };
+
   return (
     <div className="App">
-      <Select options={datas}></Select>
+      <h1>{isShow ? userSelect : ""}</h1>
+      <button onClick={() => handleSubmit()} disabled={!userSelect}>{isShow ? "Hide Button" : "Show Values"}</button>
+      <br />
+      <br />
+      <Select options={datas} onChange={(e) => handleChange(e.value)}></Select>
     </div>
   );
 }
